573f178cfdbf543bc57bff6ce55f12befbecb550
Rene Kaufmann <kaufmann.r@gmail.com>
2017-08-01 21:37:18 +0200

    redis: reuse connection and empty buffer on Stop

--- queues/redis/redis.go
+++ queues/redis/redis.go
@@ -10,124 +10,123 @@ import (
 
 // Transport is a vice.Transport for redis.
 type Transport struct {
-	sm        sync.Mutex
-	sendChans map[string]chan []byte
-
-	rm           sync.Mutex
+	sendChans    map[string]chan []byte
 	receiveChans map[string]chan []byte
+
+	*sync.Mutex
+	wg *sync.WaitGroup
 
 	errChan     chan error
 	stopchan    chan struct{}
 	stopPubChan chan struct{}
 	stopSubChan chan struct{}
 
-	clients []*redis.Client
-
-	NewClient func() *redis.Client
+	client *redis.Client
 }
 
 // New returns a new transport
-func New() *Transport {
+func New(opts ...Option) *Transport {
+	var options Options
+	for _, o := range opts {
+		o(&options)
+	}
+
 	return &Transport{
 		sendChans:    make(map[string]chan []byte),
 		receiveChans: make(map[string]chan []byte),
+		Mutex:        &sync.Mutex{},
+		wg:           &sync.WaitGroup{},
 		errChan:      make(chan error, 10),
 		stopchan:     make(chan struct{}),
 		stopPubChan:  make(chan struct{}),
 		stopSubChan:  make(chan struct{}),
-		clients:      []*redis.Client{},
-
-		NewClient: func() *redis.Client {
-			return redis.NewClient(&redis.Options{
-				Network:    "tcp",
-				Addr:       "127.0.0.1:6379",
-				Password:   "",
-				DB:         0,
-				MaxRetries: 0,
-			})
-		},
+		client:       options.Client,
 	}
 }
 
 func (t *Transport) newConnection() (*redis.Client, error) {
-	c := t.NewClient()
-	// test connection
-	_, err := c.Ping().Result()
-	if err != nil {
-		return nil, err
+	var err error
+	if t.client != nil {
+		return t.client, nil
 	}
-	return c, nil
+
+	t.client = redis.NewClient(&redis.Options{
+		Network:    "tcp",
+		Addr:       "127.0.0.1:6379",
+		Password:   "",
+		DB:         0,
+		MaxRetries: 0,
+	})
+
+	// test connection
+	_, err = t.client.Ping().Result()
+	return t.client, err
 }
 
 // Receive gets a channel on which to receive messages
 // with the specified name.
 func (t *Transport) Receive(name string) <-chan []byte {
-	t.rm.Lock()
-	defer t.rm.Unlock()
+	t.Lock()
+	defer t.Unlock()
 
 	ch, ok := t.receiveChans[name]
 	if ok {
 		return ch
 	}
 
-	c, err := t.newConnection()
+	ch, err := t.makeSubscriber(name)
 	if err != nil {
 		t.errChan <- vice.Err{Name: name, Err: err}
 		return make(chan []byte)
 	}
 
-	ch = t.makeSubscriber(c, name)
-
-	t.clients = append(t.clients, c)
 	t.receiveChans[name] = ch
 	return ch
 }
 
-func (t *Transport) makeSubscriber(c *redis.Client, name string) chan []byte {
+func (t *Transport) makeSubscriber(name string) (chan []byte, error) {
+	c, err := t.newConnection()
+	if err != nil {
+		return nil, err
+	}
+
 	ch := make(chan []byte, 1024)
 	ps, err := c.Subscribe(name)
 	if err != nil {
-		t.errChan <- vice.Err{Name: name, Err: err}
-		return make(chan []byte)
+		return make(chan []byte), err
 	}
 
 	go func() {
 		for {
-			select {
-			case <-t.stopSubChan:
-				return
-			default:
-				m, err := ps.ReceiveMessage()
-				if err != nil {
+			m, err := ps.ReceiveMessage()
+			if err != nil {
+				select {
+				case <-t.stopSubChan:
+					return
+				default:
 					t.errChan <- vice.Err{Name: name, Err: err}
 					continue
 				}
-				data := []byte(m.Payload)
-				ch <- data
 			}
+			data := []byte(m.Payload)
+			ch <- data
 		}
 	}()
-	return ch
+	return ch, nil
 }
 
 // Send gets a channel on which messages with the
 // specified name may be sent.
 func (t *Transport) Send(name string) chan<- []byte {
-	t.sm.Lock()
-	defer t.sm.Unlock()
+	t.Lock()
+	defer t.Unlock()
 
 	ch, ok := t.sendChans[name]
 	if ok {
 		return ch
 	}
 
-	c, err := t.newConnection()
-	if err != nil {
-		t.errChan <- vice.Err{Name: name, Err: err}
-		return make(chan []byte)
-	}
-
-	ch, err = t.makePublisher(c, name)
+	ch, err := t.makePublisher(name)
 	if err != nil {
 		t.errChan <- vice.Err{Name: name, Err: err}
 		return make(chan []byte)
@@ -136,20 +135,30 @@ func (t *Transport) Send(name string) chan<- []byte {
 	return ch
 }
 
-func (t *Transport) makePublisher(c *redis.Client, name string) (chan []byte, error) {
-	ch := make(chan []byte, 1024)
+func (t *Transport) makePublisher(name string) (chan []byte, error) {
+	c, err := t.newConnection()
+	if err != nil {
+		return nil, err
+	}
 
+	ch := make(chan []byte, 1024)
+	t.wg.Add(1)
 	go func() {
+		defer t.wg.Done()
 		for {
 			select {
 			case <-t.stopPubChan:
+				if len(ch) != 0 {
+					_, err := t.client.Ping().Result()
+					if err == nil {
+						continue
+					}
+				}
 				return
-
 			case msg := <-ch:
 				err := c.Publish(name, string(msg)).Err()
 				if err != nil {
 					t.errChan <- vice.Err{Message: msg, Name: name, Err: err}
-					continue
 				}
 			}
 		}
@@ -166,11 +175,10 @@ func (t *Transport) ErrChan() <-chan error {
 // The channel returned from Done() will be closed
 // when the transport has stopped.
 func (t *Transport) Stop() {
-	for _, c := range t.clients {
-		c.Close()
-	}
 	close(t.stopSubChan)
 	close(t.stopPubChan)
+	t.wg.Wait()
+	t.client.Close()
 	close(t.stopchan)
 }
 
