fa386cf3a1868e9ff377c6784ed21b46a3d3fd62
Dan Compton <dcompton@zenreach.com>
2017-08-02 10:23:49 -0700

    update backoff count to include jitter
    This commit adds jitter to the exponential backoff wrapper.

--- backoff/backoff.go
+++ backoff/backoff.go
@@ -2,34 +2,30 @@ package backoff
 
 import (
 	"log"
+	"math/rand"
 	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
 
 // Do retries the function f backing off the double of time each retry until a successfully call is made.
 // initialBackoff is minimal time to wait for the next call
 // maxBackoff is the maximum time between calls, if is 0 there is no maximum
 // maxCalls is the maximum number of call to the function, if is 0 there is no maximum
-func Do(initialBackoff, maxBackoff time.Duration, maxCalls int, f func() error) error {
-	backoff := time.Duration(0)
-	calls := 0
-	for {
-		err := f()
+func Do(min, max time.Duration, maxTries int, f func() error) error {
+	var err error
+	for rc := 0; rc < maxTries; rc++ {
+		err = f()
 		if err == nil {
 			return nil
 		}
-		calls++
-		if calls >= maxCalls && maxCalls != 0 {
-			return err
-		}
-		switch {
-		case backoff == 0:
-			backoff = initialBackoff
-		case backoff > maxBackoff && maxBackoff != 0:
-			backoff = maxBackoff
-		default:
-			backoff *= 2
-		}
-		time.Sleep(backoff)
-		log.Printf("[backoff %v] Retry after %v due to the Error: %v\n", calls, backoff, err)
+
+		delay := (1 << uint(rc)) * (rand.Intn(int(min/time.Millisecond)) + int(min/time.Millisecond))
+		st := time.Duration(delay) * time.Millisecond
+		time.Sleep(st)
+		log.Printf("[backoff %v] Retry after %v due to the Error: %v\n", rc, st, err)
 	}
+	return err
 }
