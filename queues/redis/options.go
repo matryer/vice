daa091bedf4bd2d3cff61f188b7f3c2537778fa0
ojizero <z.acnts.816@pm.me>
2022-05-19 13:23:51 +0300

    set Golint to fail if it does not pass

--- queues/redis/options.go
+++ queues/redis/options.go
@@ -2,12 +2,15 @@ package redis
 
 import "github.com/go-redis/redis"
 
+// Options is the configurations struct for the Redis transport
 type Options struct {
 	Client *redis.Client
 }
 
+// Option are functions used to setup the Options configuration struct
 type Option func(*Options)
 
+// WithClient configures Redis transport to use the given client
 func WithClient(c *redis.Client) Option {
 	return func(o *Options) {
 		o.Client = c
